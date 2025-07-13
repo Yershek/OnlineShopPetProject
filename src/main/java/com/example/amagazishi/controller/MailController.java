6cc1258dc36f50ce149fb222f8b61ac28817e64a
Yershek <homohojel3@gmail.com>
2025-07-13 15:18:13 +0600

    add Mail controller

--- src/main/java/com/example/amagazishi/controller/MailController.java
+++ src/main/java/com/example/amagazishi/controller/MailController.java
@@ -1,4 +1,28 @@
 package com.example.amagazishi.controller;
 
+import com.example.amagazishi.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+@RequiredArgsConstructor
 public class MailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/send-message-to-mail")
+    public ResponseEntity<String> sendMassage(
+            @RequestParam String email,
+            @RequestParam String title,
+            @RequestParam String content
+    ){
+        mailService.sendMessageTo(email,title,content);
+        return ResponseEntity.ok("ваще сообщение отправлено");
+    }
+
 }
